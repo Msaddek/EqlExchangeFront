9082bbb16e8f9554e675edf7324a046e4e2875a0
pharaphara <alexander.deltour@gmail.com>
2022-02-17 15:15:17 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiWalletUrl: process.env["walletapp_URL"],
+  apiWalletUrl: 'walletapp_URL',
   backEnd: 'http://3legacy-env.eba-3dibhvqm.eu-west-3.elasticbeanstalk.com',
-  apiMatchingEngine:  process.env["matchengine_URL"],
+  apiMatchingEngine:  'matchengine_URL',
   backLambda: 'https://v7oflitra5.execute-api.eu-west-3.amazonaws.com/prod'
 };
 
