4eff7f9265f8221cfc83bb6b4ad54a217449c530
Msaddek <basem.msaddek@gmail.com>
2022-02-18 14:35:50 +0100

    fix URL prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiWalletUrl: 'http://walletapp_URL',
+  apiWalletUrl: 'http://walletapp_URL/assets-api-rest',
   backEnd: 'http://3legacy-env.eba-3dibhvqm.eu-west-3.elasticbeanstalk.com',
   apiMatchingEngine:  'http://matchengine_URL',
   backLambda: 'https://v7oflitra5.execute-api.eu-west-3.amazonaws.com/prod'
